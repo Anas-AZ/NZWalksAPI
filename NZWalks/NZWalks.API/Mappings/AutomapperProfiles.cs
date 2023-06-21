1da469f19cb5fd140048fc1f0099a1c53a246b6e
Anas <anas9991az@gmail.com>
2023-06-21 17:19:15 +0530

    Navigation Properties in EF Core

--- NZWalks/NZWalks.API/Mappings/AutomapperProfiles.cs
+++ NZWalks/NZWalks.API/Mappings/AutomapperProfiles.cs
@@ -13,6 +13,9 @@ namespace NZWalks.API.Mappings
             CreateMap<UpdateRegionRequestDTO, Region>().ReverseMap();
             CreateMap<AddWalkRequestDTO, Walk>().ReverseMap();
             CreateMap<Walk, WalkDTO>().ReverseMap();
+            CreateMap<Difficulty, DifficultyDTO>().ReverseMap();
+
+
 
         }
     }
--- NZWalks/NZWalks.API/Models/DTO/WalkDTO.cs
+++ NZWalks/NZWalks.API/Models/DTO/WalkDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace NZWalks.API.Models.DTO
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Models.DTO
 {
     public class WalkDTO
     {
@@ -7,9 +9,7 @@
         public string Description { get; set; }
         public double LengthInKm { get; set; }
         public string? WalkImageUrl { get; set; }
-
-        public Guid DifficultyId { get; set; }
-
-        public Guid RegionId { get; set; }
+        public RegionDTO Region { get; set; }
+        public DifficultyDTO Difficulty { get; set; }
     }
 }
